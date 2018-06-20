f5a14854e8675b1fc5a2b6af027aa19aee2db893
TrueGardener555 <39525992+TrueGardener555@users.noreply.github.com>
2018-06-20 23:47:25 +0700

    Add files via upload

---
+++ Automative.py
@@ -0,0 +1,15 @@
+import shutil
+import os
+
+screenshotsDir= "C:\\"
+img = capture()
+shutil.move(img, os.path.join(screenshotsDir, xx.png))
+
+click("windowsbutton.PNG")
+paste("C:\Program Files (x86)\Walther Schulze\My Product Name")
+
+type(Key.ENTER)
+
+doubleClick("1527163779200.png")
+
+click("Registerbutton.PNG")
